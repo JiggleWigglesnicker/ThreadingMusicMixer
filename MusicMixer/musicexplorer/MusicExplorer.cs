1c963514bc03b5628e925e3a83caf63d22fc9d46
Prime Knight <games.j@live.nl>
2020-05-18 20:52:09 +0200

    improved musicexplorer

--- MusicMixer/musicexplorer/MusicExplorer.cs
+++ MusicMixer/musicexplorer/MusicExplorer.cs
@@ -1,20 +1,47 @@
-﻿using System;
+﻿using MusicMixer.musicplayer;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Audio;
 
 namespace MusicMixer.musicexplorer
 {
     class MusicExplorer
     {
-        public MusicExplorer() {
-        
+
+        private HashSet<MusicFile> musicList = new HashSet<MusicFile>();
+
+        public MusicExplorer()
+        {
+            CreateMusicFolder();
         }
 
-        public bool CheckIfMusicFolderExist() {
+        public bool CheckIfMusicFolderExist()
+        {
             return Directory.Exists("C:\\Users\\Prime Knight\\source\repos\\ThreadingMusicMixer\\MusicMixer\\MusicFolder");
         }
+
+        public void CreateMusicFolder()
+        {
+            if (!CheckIfMusicFolderExist())
+            {
+                Directory.CreateDirectory("C:\\Users\\Prime Knight\\source\repos\\ThreadingMusicMixer\\MusicMixer\\MusicFolder");
+            }
+            else
+            {
+                Console.WriteLine("Musicfolder already exist");
+            }
+
+        }
+
+        public void FindNewMusic()
+        {
+
+        }
+
     }
 }
