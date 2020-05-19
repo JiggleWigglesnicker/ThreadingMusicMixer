48e0b6fe3d2c1d6905d90f6950b7182aa20744b0
Sander111 <sander.paping@student.stenden.nl>
2020-05-19 19:33:15 +0200

    threadpool update

--- MusicMixer/threadpool/threadpool.cs
+++ MusicMixer/threadpool/threadpool.cs
@@ -8,5 +8,19 @@ namespace MusicMixer.threadpool
 {
     class threadpool
     {
+<<<<<<< HEAD
+=======
+        private static void
+            TaskCallBack(Object ThreadNumber)
+        {
+            string ThreadName = "Thread " +
+                ThreadNumber.ToString();
+            for (int i = 1; i < 10; i++)
+                Console.WriteLine(ThreadName
+                    + ": " + i.ToString());
+            Console.WriteLine(ThreadName
+                + "Finished...");
+        }
+>>>>>>> parent of 7b17791... Delete threadpool
     }
 }
