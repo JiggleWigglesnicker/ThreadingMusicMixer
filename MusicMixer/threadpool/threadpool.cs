3eb62ee93a428b4e2312d36cfadfe334d5ca83e6
Sander111 <sanderpaping@live.nl>
2020-05-19 19:23:27 +0200

    change threadpool

--- MusicMixer/threadpool/threadpool.cs
+++ MusicMixer/threadpool/threadpool.cs
@@ -8,5 +8,16 @@ namespace MusicMixer.threadpool
 {
     class threadpool
     {
+        private static void
+            TaskCallBack(Object ThreadNumber)
+        {
+            string ThreadName = "Thread " +
+                ThreadNumber.ToString();
+            for (int i = 1; i < 10; i++)
+                Console.WriteLine(ThreadName
+                    + ": " + i.ToString());
+            Console.WriteLine(ThreadName
+                + "Finished...");
+        }
     }
 }
